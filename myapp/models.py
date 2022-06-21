f7c881b81995fcb0b987ce6a700927b8d3d9d470
Moringa School <calebmutuku4@gmail.com>
2022-06-21 22:12:43 +0300

    added the data models for ratings and companies

--- myapp/models.py
+++ myapp/models.py
@@ -25,3 +25,86 @@ class Profile(models.Model):
     @receiver(post_save,sender=User) 
     def save_user_profile(sender,instance,**kwargs):
         instance.profile.save()
+        
+class Category(models.Model):
+    category_name=models.CharField(max_length=30,unique=True)
+
+    def __str__(self):
+        return self.category_name
+
+    def save_category(self):
+        self.save 
+    def delete_category(self):
+        self.delete   
+
+class Companies(models.Model):
+    name=models.CharField(max_length=160)
+    website=models.URLField(max_length=300)
+    about=models.TextField(max_length=300)
+    size=models.IntegerField()
+    logo=CloudinaryField('image')
+    location=models.CharField(max_length=100,blank=True)
+    camp_category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    user=models.ForeignKey(User,on_delete=models.CASCADE,related_name='posts')
+    date=models.DateTimeField(auto_now_add=True,blank=True)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    def save_post(self):
+        self.save()
+
+    def delete_post(self):
+        self.delete()
+
+    @classmethod
+    def all_posts(cls):
+        return cls.objects.all()
+
+
+    @classmethod
+    def search_projects(cls,name):
+        return cls.objects.filter(name__icontains=name).all()  
+
+    @classmethod
+    def search_category(cls,search_term) :
+        search_results = cls.objects.filter(camp_category__category_name__icontains=search_term)
+        return search_results
+
+class Rating(models.Model):
+    rating = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+    )
+
+    company_culture = models.IntegerField(choices=rating, default=0, blank=True)
+    skill_development = models.IntegerField(choices=rating, blank=True)
+    work_life_balance = models.IntegerField(choices=rating, blank=True)
+    work_satisfaction = models.IntegerField(choices=rating, blank=True)
+    job_security = models.IntegerField(choices=rating, blank=True)
+    salary_benefits = models.IntegerField(choices=rating, blank=True)
+    career_growth = models.IntegerField(choices=rating, blank=True)
+    score = models.FloatField(default=0, blank=True)
+    company_culture_average = models.FloatField(default=0, blank=True)
+    skill_development_average = models.FloatField(default=0, blank=True)
+    work_life_balance_average = models.FloatField(default=0, blank=True)
+    work_satisfaction_average = models.FloatField(default=0, blank=True)
+    job_security_average = models.FloatField(default=0, blank=True)
+    salary_benefits_average = models.FloatField(default=0, blank=True)
+    career_growth_average = models.FloatField(default=0, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='rater')
+    company = models.ForeignKey(Companies, on_delete=models.CASCADE, related_name='ratings', null=True)
+
+    def save_rating(self):
+        self.save()
+
+    @classmethod
+    def get_ratings(cls, id):
+        ratings = Rating.objects.filter(post_id=id).all()
+        return ratings
+
+    def __str__(self):
+        return f'{self.post} Rating'
