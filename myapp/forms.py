032d47a42731a585f10d9f2debeeb6c05a799100
Moringa School <calebmutuku4@gmail.com>
2022-06-22 02:50:49 +0300

    ft:added the rating form

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,9 +1,12 @@
 from django import forms
-from .models import  Profile
+from .models import  Profile,Companies,Rating
 from django.contrib.auth.models import User
 
 
-
+class PostCompanyForm(forms.ModelForm):
+    class Meta:
+        model=Companies
+        exclude = ('user','date')
 
 class UpdateUserForm(forms.ModelForm):
     email=forms.EmailField(max_length=254,help_text='Required.Inform a valid email addres')
@@ -16,3 +19,9 @@ class UpdateUserProfileForm(forms.ModelForm):
     class Meta:
         model= Profile
         fields=('name','bio','profile_picture','location','contact_email')
+
+
+class RatingsForm(forms.ModelForm):
+    class Meta:
+        model = Rating
+        fields = ['company_culture', 'skill_development', 'work_life_balance','work_satisfaction','job_security','salary_benefits','career_growth']
