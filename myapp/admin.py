6922ef542637f7a8a912cd5317d4d8a5f1d995ac
Moringa School <calebmutuku4@gmail.com>
2022-06-22 22:59:20 +0300

    registred the review model in the admin.py

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
-from .models import Profile, Companies
+from .models import Profile, Companies,Review
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Companies)
-# admin.site.register(Category)
+admin.site.register(Review)
