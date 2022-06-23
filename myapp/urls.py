7540efbea3e15ccde911bf6a2d244eec5feabcf0
Moringa School <calebmutuku4@gmail.com>
2022-06-23 23:22:34 +0300

    added the uls for comment and like function

--- myapp/urls.py
+++ myapp/urls.py
@@ -13,18 +13,12 @@ urlpatterns=[
     re_path('update', views.update_profile, name='update'),
     re_path('new-camp/', views.postcompany, name='newcampany'),
     re_path('newblog/', views.postblog, name='newblog'),
-
     re_path(r'^project/(?P<post_id>\d+)?$', views.project, name='project'), 
     re_path(r'^review/(?P<company_id>\d+)?$', views.review, name='review'),
-    re_path(r'^singleblog/(?P<blog_id>\d+)?$', views.singleblog, name='singleblog'), 
-
-    # re_path('review/', views.review, name='review'),
-    # re_path('review/<int:pk>' , views.review, name='comment'),
-
- 
-
-
-
+    re_path(r'^singleblog/(?P<blog_id>\d+)?$', views.singleblog, name='singleblog'),
+    re_path(r'^comment/(?P<post_id>\d+)?$', views.comment, name='comment'),
+    re_path(r'^like_post/(?P<blog_id>\d+)?$', views.like_post, name='like_post'),
+    re_path('search/', views.search_company, name='search'),
 
 ]
 if settings.DEBUG:
