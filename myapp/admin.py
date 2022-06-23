b81dde3be01887404642aec8fd42dcec235d59da
Moringa School <calebmutuku4@gmail.com>
2022-06-23 23:23:44 +0300

    registred the comment and likes models in the admin.py

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Profile, Companies,Review
+from .models import Profile, Companies,Review,Comment
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Companies)
 admin.site.register(Review)
+admin.site.register(Comment)
